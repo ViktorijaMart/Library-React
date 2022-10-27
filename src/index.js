9b2c6ccf248f6c7e5b43869ebfc81f57764e4c1a
LhunaCat <viktorijamartinaityte@gmail.com>
2022-10-27 16:17:14 +0300

    Home page done

--- src/index.js
+++ src/index.js
@@ -8,9 +8,23 @@ import {
   faBars,
   faShoppingCart,
   faTimes,
+  faBolt,
+  faBookOpen,
+  faTags,
+  faStar,
+  faStarHalfAlt,
 } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faBars, faShoppingCart, faTimes);
+library.add(
+  faBars,
+  faShoppingCart,
+  faTimes,
+  faBolt,
+  faBookOpen,
+  faTags,
+  faStar,
+  faStarHalfAlt
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
