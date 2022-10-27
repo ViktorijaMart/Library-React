12aa1a5fa8fbabb67ac9a6ab1b392b9ce403b24b
LhunaCat <viktorijamartinaityte@gmail.com>
2022-10-27 17:17:45 +0300

    Books page done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore";
-import Featured from "./components/Featured";
 import Footer from "./components/Footer";
-import Highlights from "./components/Highlights";
-import Landing from "./components/Landing";
 import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/books" element={<Books books={books} />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
